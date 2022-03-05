01961c0fd407175fa646279866279ff426208c06
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2022-03-05 15:04:22 +0100

    Modification CSS et images

--- tamagoshi/graphic/TamaPane.java
+++ tamagoshi/graphic/TamaPane.java
@@ -4,38 +4,49 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
 import tamagoshi.tamagoshis.Tamagoshi;
 
 import java.util.Objects;
 
 public class TamaPane extends BorderPane {
     private final Tamagoshi tamagoshi;
-    private String message;
     private final Label labelNom;
+    private Label labelMessage;
     private ImageView imageView;
 
     public TamaPane(Tamagoshi tamagoshi) {
         this.tamagoshi = tamagoshi;
         this.labelNom = new Label(tamagoshi.getName());
         this.labelNom.getStyleClass().add("label");
+        this.labelMessage = new Label();
+        this.imageView = new ImageView();
+        this.labelMessage.setWrapText(true);
+        this.labelMessage.getStyleClass().add("message");
         this.updatePhase();
         this.setTop(this.labelNom);
-        this.setBottom(new Text(message));
     }
 
     private void getPhase() {
-        if (this.getTamagoshi().getEnergy() > 4 && this.getTamagoshi().getFun() > 4) {
-            this.imageView = new ImageView(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_1.jpg"))));
-        } else if (this.getTamagoshi().getEnergy() <= 4 && this.getTamagoshi().getFun() <= 4) {
-            this.imageView = new ImageView(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_3.png"))));
-        } else if (this.getTamagoshi().getEnergy() <= 4 || this.getTamagoshi().getFun() <= 4) {
-            this.imageView = new ImageView(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_2.jpg"))));
-        } else if (this.getTamagoshi().getEnergy() <= 0 || this.getTamagoshi().getFun() <= 0) {
-            this.imageView = new ImageView(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_4.png"))));
-        } else if (this.getTamagoshi().getAge() >= Tamagoshi.getLifeTime()) {
-            this.imageView = new ImageView(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_0.png"))));
+        int energy = this.getTamagoshi().getEnergy();
+        int fun = this.getTamagoshi().getFun();
+
+        if (this.getTamagoshi().getAge() >= Tamagoshi.getLifeTime()) {
+            this.getImageView().setImage(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_0.png"))));
+
+        } else if (energy <= 0 || fun <= 0) {
+            this.getImageView().setImage(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_4.png"))));
+
+        } else if (energy > 4 && fun > 4) {
+            this.getImageView() .setImage(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_1.png"))));
+
+        } else if ((energy <= 4 && fun > 4) || (energy > 4 && fun <= 4)) {
+            this.getImageView().setImage(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_2.png"))));
+
+        } else if (energy <= 4 && fun <= 4) {
+            this.getImageView().setImage(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/tamagoshi/images/phase_3.png"))));
         }
+
+        System.out.println(this.getTamagoshi());
     }
 
     protected void manger() {
@@ -56,10 +67,16 @@ public class TamaPane extends BorderPane {
         this.getImageView().setFitWidth(400);
         this.getImageView().setFitHeight(400);
         this.setCenter(this.getImageView());
+        this.getCenter().getStyleClass().add("customImageView");
     }
 
     private void updateMessage(String message) {
-        this.setBottom(new Text(message));
+        if (this.getTamagoshi().getAge() < Tamagoshi.getLifeTime()) {
+            this.getLabelMessage().setText(message);
+        } else {
+            this.getLabelMessage().setText("Merci !");
+        }
+        this.setBottom(this.getLabelMessage());
     }
 
     // Getters et setters
@@ -72,11 +89,7 @@ public class TamaPane extends BorderPane {
         return imageView;
     }
 
-    private String getMessage() {
-        return message;
-    }
-
-    private void setMessage(String message) {
-        this.message = message;
+    public Label getLabelMessage() {
+        return labelMessage;
     }
 }
