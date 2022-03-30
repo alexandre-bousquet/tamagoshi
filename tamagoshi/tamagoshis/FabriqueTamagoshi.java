81cd646bedb3eba592f22f7b5c90d30070f0449f
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2022-03-30 22:43:11 +0200

    Commentaires

--- tamagoshi/tamagoshis/FabriqueTamagoshi.java
+++ tamagoshi/tamagoshis/FabriqueTamagoshi.java
@@ -4,15 +4,22 @@ import tamagoshi.jeu.TamaGame;
 
 import java.util.*;
 
+/**
+ * Patron de conception fabrique concrete qui permet de ne pas modifier le code de l'application en cas d'ajout d'un Tamagoshi sauf au niveau de la méthode {@link FabriqueTamagoshi#generateRandomTamagoshi()}.
+ */
 public class FabriqueTamagoshi {
     private static FabriqueTamagoshi instance;
     private final List<String> listeNomTamagoshi;
 
-    public FabriqueTamagoshi() {
+    private FabriqueTamagoshi() {
         listeNomTamagoshi = new ArrayList<>();
         this.initNamesList();
     }
 
+    /**
+     * Patron de conception Singleton qui permet d'avoir qu'une seule instance possible de la fabrique.
+     * @return La seule et unique instance de la fabrique (si elle existe pas elle est créée)
+     */
     public static FabriqueTamagoshi getInstance() {
         if (instance == null) {
             instance = new FabriqueTamagoshi();
@@ -34,7 +41,7 @@ public class FabriqueTamagoshi {
     }
 
     /**
-     * Génère un tamagoshi aléatoire.
+     * Génère un tamagoshi aléatoire avec un nom aléatoire en fonction de probabilités établies par le programmeur.
      * @return Le tamagoshi généré.
      */
     public static Tamagoshi generateRandomTamagoshi() {
